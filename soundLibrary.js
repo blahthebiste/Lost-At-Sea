c0e4e8b31267ddc11b7fe730443c615927712306
blahthebiste <eliezermiron116@gmail.com>
2016-06-09 19:44:40 -0700

    Fixed Music restarting randomly

--- soundLibrary.js
+++ soundLibrary.js
@@ -1,23 +1,23 @@
 	var stage1Music = new Howl({
 		urls: ['Sounds/LaS-Stage1.ogg'],
-		loop: true,
-		buffer: true,
+		//loop: true,
+		//buffer: true,
 		onend: function(){
 			console.log("Restarting loop. Ended at position",this.pos());
 		}
 	});
 	var stage2Music = new Howl({
 		urls: ['Sounds/LaS-Stage2.ogg'],
-		loop: true,
-		buffer: true,
+		//loop: true,
+		//buffer: true,
 		onend: function(){
 			console.log("Restarting loop. Ended at position",this.pos());
 		}
 	});
 	var stage3Music = new Howl({
 		urls: ['Sounds/LaS-Stage3.ogg'],
-		loop: true,
-		buffer: true,
+		//loop: true,
+		//buffer: true,
 		onend: function(){
 			console.log("Restarting loop. Ended at position",this.pos());
 		}
